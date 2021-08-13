a7a8705a9339f5ecc9e50d13c0446dfb47a2ebcf
ComradEvg <88490590+ComradEvg@users.noreply.github.com>
2021-08-13 15:32:35 +0300

    ver:1.25
    Почти сделана отправка на почту

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-$(document).ready(function () {
-
-   $(function(){
-      $('.preloader, .loader').addClass('complete');
-      setTimeout(function () {$('.preloader').remove()},1000)
-   });
+"use strick"
+document.addEventListener('DOMContentLoaded', function () {
 
    document.querySelectorAll('a.menu__link').forEach(link => {
-      link.addEventListener('click', function (e){
+      link.addEventListener('click', function (e) {
          e.preventDefault()
          const href = this.getAttribute('href').substring(1)
          const scrollTarget = document.getElementById(href)
@@ -22,20 +18,92 @@ $(document).ready(function () {
       })
    });
 
-   window.addEventListener('scroll', () =>{
+   window.addEventListener('scroll', () => {
       let scrollDistance = window.scrollY - document.querySelector('.header').clientHeight;
-      console.log(scrollDistance);
       document.querySelectorAll('.anchor').forEach((el, i) => {
-         if(el.offsetTop<=scrollDistance){
-            console.log(el.offsetTop);
-            document.querySelectorAll('.menu__list a').forEach((el)=>{
-               if(el.classList.contains('active-nav')){
+         if (el.offsetTop <= scrollDistance) {
+            document.querySelectorAll('.menu__list a').forEach((el) => {
+               if (el.classList.contains('active-nav')) {
                   el.classList.remove('active-nav');
                }
             });
             document.querySelectorAll('.menu__list li')[i].querySelector('a').classList.add('active-nav');
          }
       });
+   });
+
+   const form = document.getElementById("form");
+   const loaderform = document.getElementById('loader-form');
+   form.addEventListener('submit', formSend);
+
+
+   async function formSend(e) {
+      e.preventDefault();
+
+      let error = formValidate(form);
+
+      let formData = new FormData(form);
+
+      if (error === 0) {
+         loaderform.classList.add('_sending');
+         let response = await fetch('./sendmail.php', {
+            method: 'POST',
+            body: formData
+         });
+         if (response.ok) {
+            let result = await response.json();
+            alert(result.message);
+            form.reset();
+            loaderform.classList.remove('_sending');
+         }
+         else {
+            alert("Ошибка");
+            loaderform.classList.remove('_sending');
+         }
+      }
+      else {
+         alert('Заполните обязательные поля');
+      }
+   }
+
+   function formValidate(form) {
+      let error = 0;
+      let formReq = document.querySelectorAll('._req');
+      for (let index = 0; index < formReq.length; index++) {
+         const input = formReq[index];
+         formRemoveError(input);
+
+         if (input.classList.contains('_email')) {
+            if (emailTest(input)) {
+               formAddError(input);
+               error++;
+            }
+         } else {
+            if (input.value === '') {
+               formAddError(input);
+               error++;
+            }
+         }
+      }
+      return error;
+   }
+
+   function formAddError(input) {
+      input.parentElement.classList.add('_error');
+      input.classList.add('_error');
+   }
+   function formRemoveError(input) {
+      input.parentElement.classList.remove('_error');
+      input.classList.remove('_error');
+   }
+   // Function test Email
+   function emailTest(input) {
+      return !/^\w+([\.-]?\w+)*@\w([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+   }
+
+   $(function () {
+      $('.preloader, .loader').addClass('complete');
+      setTimeout(function () { $('.preloader').remove() }, 1000)
    });
 
    $('.burger').on('click', function (event) {
@@ -48,11 +116,10 @@ $(document).ready(function () {
       $('.card__marker').remove();
    });
 
-   
+
 
    $('.social__link').hover(function (event) {
       $(this).find('.box-bw').toggleClass('disactive-icon');
       $(this).find(':first').toggleClass('active-icon');
    });
-
 });
---
+++ sendmail.php
@@ -0,0 +1,45 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPmMailer.php';
+
+$mail = new PHPMailer(true);
+$mail ->CharSet = 'UTF-8';
+$mail ->setLanguage('ru', 'phpmailer/languadge/');
+$mail ->isHTML(true);
+
+$mail->setFrom('stekolnikove@mail.ru');
+$mail ->addAddress('stekolnikove@mail.ru');
+$mail ->Subject = 'Проверка почты';
+
+
+// Тело письма
+$body = '<h1>Встречайте супер письмо!</h1>';
+
+if(trim(!empty($_POST['name']))){
+   $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+}
+if (trim(!empty($_POST['email']))) {
+   $body .= '<p><strong>E-mail:</strong> ' . $_POST['email'] . '</p>';
+}
+if (trim(!empty($_POST['message']))) {
+   $body .= '<p><strong>Сообщение:</strong> ' . $_POST['message'] . '</p>';
+}
+
+$mail -> Body = $body;
+
+// SEND
+if (!$mail->send()){
+   $message = 'Ошибка';
+}
+else{
+   $message = 'Сообщение отправлено';
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
+?>
