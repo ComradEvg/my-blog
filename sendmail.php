06655d067f49e9c44b8e2b8ef42149da2ab53798
ComradEvg <88490590+ComradEvg@users.noreply.github.com>
2021-08-13 16:04:30 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,9 +1,12 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 $mail ->CharSet = 'UTF-8';
@@ -42,4 +45,5 @@ $response = ['message' => $message];
 
 header('Content-type: application/json');
 echo json_encode($response);
+
 ?>
